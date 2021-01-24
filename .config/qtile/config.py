97c396c34aebeaf1f870293a2ffe6ac81e0463c6
Hassen Taidirt <htaidirt@gmail.com>
2021-01-24 15:03:38 +0100

    Hello qtile

---
+++ .config/qtile/config.py
@@ -0,0 +1,135 @@
+from typing import List  # noqa: F401
+
+from libqtile import bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+mod = "mod4"
+terminal = guess_terminal()
+
+keys = [
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "space", lazy.spawn("launcher"), desc="Start lancher"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart qtile"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown qtile"),
+
+    Key([mod], "h", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "l", lazy.layout.left(), desc="Move focus to left"),
+
+    Key([mod, "control"], "h", lazy.layout.grow_right(), desc="Grow to right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow up"),
+    Key([mod, "control"], "l", lazy.layout.grow_left(), desc="Grow to left"),
+
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move to left"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move up"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move to right"),
+
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
+
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+
+    Key([mod], "semicolon", lazy.spawn("lock"), desc="Lock screen"),
+    Key([mod], "a", lazy.spawn("skippy-xd"), desc="Show apps like Mission Control"),
+
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("volume up")),
+    Key([], "XF86AudioLowerVolume", lazy.spawn("volume down")),
+    Key([], "XF86AudioMute", lazy.spawn("volume toggle")),
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightness up")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightness down")),
+]
+
+groups = [Group(i) for i in "1234567890"]
+
+for i in groups:
+    keys.extend([
+        Key([mod], i.name, lazy.group[i.name].toscreen(), desc="Switch to group {}".format(i.name)),
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
+    ])
+
+layouts = [
+    layout.MonadTall(),
+    layout.Columns(border_focus_stack='#0090ff'),
+    layout.Max(),
+]
+
+widget_defaults = dict(
+    font='sans',
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.Systray(),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+            ],
+            24,
+        ),
+    ),
+]
+
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+main = None  # WARNING: this is deprecated and will be removed soon
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    {'wmclass': 'confirm'},
+    {'wmclass': 'dialog'},
+    {'wmclass': 'download'},
+    {'wmclass': 'error'},
+    {'wmclass': 'file_progress'},
+    {'wmclass': 'notification'},
+    {'wmclass': 'splash'},
+    {'wmclass': 'toolbar'},
+    {'wmclass': 'confirmreset'},  # gitk
+    {'wmclass': 'makebranch'},  # gitk
+    {'wmclass': 'maketag'},  # gitk
+    {'wname': 'branchdialog'},  # gitk
+    {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~')
+    subprocess.call([home + '/.config/qtile/autostart.sh'])
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
